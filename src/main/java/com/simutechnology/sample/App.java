14726fe4e6032c98a427dd2662ffff445380350d
takatsugu ishikawa <tissi0708@gmail.com>
2015-02-14 21:07:53 +0900

    add App.java as a example

---
+++ src/main/java/com/simutechnology/sample/App.java
@@ -0,0 +1,15 @@
+package com.simutechnology.sample;
+
+/**
+ * Created by ishi on 15/02/14.
+ */
+public class App {
+
+    public static String getMessage() {
+        return "Hello Gradle";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(App.getMessage());
+    }
+}
